ab61a88bf5b2ef9d48d8a5593477aa9ef090f816
srinivas1043 <sm1043@gmail.com>
2021-10-04 21:39:53 +0200

    Add menu component resources and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { CommunityComponent } from './community/community.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    AboutComponent,
+    TechnologiesComponent,
+    SolutionsComponent,
+    CommunityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
